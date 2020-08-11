ae771a46cd166402dcae6afb369096fa2a9b67da
DSDi <>
2020-08-11 13:28:44 +0200

    input length fix

--- main.js
+++ main.js
@@ -131,6 +131,16 @@ let amountElementValue = amountElement.value;
         resultElement.innerText = `${amountElementValue} ${FirstCurrencyName} = ${result} ${CurrencyName}`;
     };
 
+    function enforce_maxlength(event) {
+        var t = event.target;
+        if (t.hasAttribute('maxlength')) {
+          t.value = t.value.slice(0, t.getAttribute('maxlength'));
+        }
+      }
+
+    document.body.addEventListener('input', enforce_maxlength);
+
+
     currencyFirstElement.addEventListener("input", exchange);
     currencySecondElement.addEventListener("input", exchange);
     amountElement.addEventListener("input", exchange);
