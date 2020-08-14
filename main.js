4baa2f87a16e30b968ab85159b2f0050edd6f7d2
DSDi <>
2020-08-14 14:32:23 +0200

    js fix

--- main.js
+++ main.js
@@ -1,16 +1,9 @@
-
-const amountElement = document.querySelector(".js-amount");
-const resultElement = document.querySelector(".js-result");
-
-const currencyFirstElement = document.querySelector(".js-currency-first");
-const currencySecondElement = document.querySelector(".js-currency-second");
-
-const amountElementValue = amountElement.value;
-
-
+{
+  let exchange = () => {
+      
     const ratePLN = 1;
     const rateWON = 0.003156;
-    const rateJUAN = 0.5380;
+    const rateJUAN = 0.538;
     const rateJEN = 0.035237;
     const rateRINGGIT = 0.8907;
     const rateBAHT = 0.1203;
@@ -21,126 +14,130 @@ const amountElementValue = amountElement.value;
     const rateRIEL = 0.00091;
     const rateKIP = 0.00041;
 
+    switch (currencyFirstElement.value) {
+      case "PLN":
+        FirstCurrencyType = ratePLN;
+        FirstCurrencyName = "PLN";
+        break;
+      case "KRW":
+        FirstCurrencyType = rateWON;
+        FirstCurrencyName = "KRW";
+        break;
+      case "CNY":
+        FirstCurrencyType = rateJUAN;
+        FirstCurrencyName = "CNY";
+        break;
+      case "JPY":
+        FirstCurrencyType = rateJEN;
+        FirstCurrencyName = "JPY";
+        break;
+      case "MYR":
+        FirstCurrencyType = rateRINGGIT;
+        FirstCurrencyName = "MYR";
+        break;
+      case "THB":
+        FirstCurrencyType = rateBAHT;
+        FirstCurrencyName = "THB";
+        break;
+      case "PHP":
+        FirstCurrencyType = ratePESO;
+        FirstCurrencyName = "PHP";
+        break;
+      case "SGD":
+        FirstCurrencyType = rateSINGAPUR;
+        FirstCurrencyName = "SGD";
+        break;
+      case "HKD":
+        FirstCurrencyType = rateHONG;
+        FirstCurrencyName = "HKD";
+        break;
+      case "IDR":
+        FirstCurrencyType = rateRUPIA;
+        FirstCurrencyName = "IDR";
+        break;
+      case "KHR":
+        FirstCurrencyType = rateRIEL;
+        FirstCurrencyName = "KHR";
+        break;
+      case "LAK":
+        FirstCurrencyType = rateKIP;
+        FirstCurrencyName = "LAK";
+        break;
+    }
 
-    let exchange = () => {
-        switch (currencyFirstElement.value) {
-            case "PLN":
-                FirstCurrencyType = ratePLN;
-                FirstCurrencyName = "PLN";
-                break;
-            case "KRW":
-                FirstCurrencyType = rateWON;
-                FirstCurrencyName = "KRW";
-                break;
-            case "CNY":
-                FirstCurrencyType = rateJUAN;
-                FirstCurrencyName = "CNY";
-                break;
-            case "JPY":
-                FirstCurrencyType = rateJEN;
-                FirstCurrencyName = "JPY";
-                break;
-            case "MYR":
-                FirstCurrencyType = rateRINGGIT;
-                FirstCurrencyName = "MYR";
-                break;
-            case "THB":
-                FirstCurrencyType = rateBAHT;
-                FirstCurrencyName = "THB";
-                break;
-            case "PHP":
-                FirstCurrencyType = ratePESO;
-                FirstCurrencyName = "PHP";
-                break;
-            case "SGD":
-                FirstCurrencyType = rateSINGAPUR;
-                FirstCurrencyName = "SGD";
-                break;
-            case "HKD":
-                FirstCurrencyType = rateHONG;
-                FirstCurrencyName = "HKD";
-                break;
-            case "IDR":
-                FirstCurrencyType = rateRUPIA;
-                FirstCurrencyName = "IDR";
-                break;
-            case "KHR":
-                FirstCurrencyType = rateRIEL;
-                FirstCurrencyName = "KHR";
-                break;
-            case "LAK":
-                FirstCurrencyType = rateKIP;
-                FirstCurrencyName = "LAK";
-                break;
-        }
+    switch (currencySecondElement.value) {
+      case "PLN":
+        SecondCurrencyType = ratePLN;
+        CurrencyName = "PLN";
+        break;
+      case "KRW":
+        SecondCurrencyType = rateWON;
+        CurrencyName = "KRW";
+        break;
+      case "CNY":
+        SecondCurrencyType = rateJUAN;
+        CurrencyName = "CNY";
+        break;
+      case "JPY":
+        SecondCurrencyType = rateJEN;
+        CurrencyName = "JPY";
+        break;
+      case "MYR":
+        SecondCurrencyType = rateRINGGIT;
+        CurrencyName = "MYR";
+        break;
+      case "THB":
+        SecondCurrencyType = rateBAHT;
+        CurrencyName = "THB";
+        break;
+      case "PHP":
+        SecondCurrencyType = ratePESO;
+        CurrencyName = "PHP";
+        break;
+      case "SGD":
+        SecondCurrencyType = rateSINGAPUR;
+        CurrencyName = "SGD";
+        break;
+      case "HKD":
+        SecondCurrencyType = rateHONG;
+        CurrencyName = "HKD";
+        break;
+      case "IDR":
+        SecondCurrencyType = rateRUPIA;
+        CurrencyName = "IDR";
+        break;
+      case "KHR":
+        SecondCurrencyType = rateRIEL;
+        CurrencyName = "KHR";
+        break;
+      case "LAK":
+        SecondCurrencyType = rateKIP;
+        CurrencyName = "LAK";
+        break;
+    }
+    
+    const result = ((amountElement.value * FirstCurrencyType) /SecondCurrencyType).toLocaleString();
+    resultElement.innerText = `${amountElement.value} ${FirstCurrencyName} = ${result} ${CurrencyName}`;
+  };
 
-        switch (currencySecondElement.value) {
-            case "PLN":
-                SecondCurrencyType = ratePLN;
-                CurrencyName = "PLN";
-                break;
-            case "KRW":
-                SecondCurrencyType = rateWON;
-                CurrencyName = "KRW";
-                break;
-            case "CNY":
-                SecondCurrencyType = rateJUAN;
-                CurrencyName = "CNY";
-                break;
-            case "JPY":
-                SecondCurrencyType = rateJEN;
-                CurrencyName = "JPY";
-                break;
-            case "MYR":
-                SecondCurrencyType = rateRINGGIT;
-                CurrencyName = "MYR";
-                break;
-            case "THB":
-                SecondCurrencyType = rateBAHT;
-                CurrencyName = "THB";
-                break;
-            case "PHP":
-                SecondCurrencyType = ratePESO;
-                CurrencyName = "PHP";
-                break;
-            case "SGD":
-                SecondCurrencyType = rateSINGAPUR;
-                CurrencyName = "SGD";
-                break;
-            case "HKD":
-                SecondCurrencyType = rateHONG;
-                CurrencyName = "HKD";
-                break;
-            case "IDR":
-                SecondCurrencyType = rateRUPIA;
-                CurrencyName = "IDR";
-                break;
-            case "KHR":
-                SecondCurrencyType = rateRIEL;
-                CurrencyName = "KHR";
-                break;
-            case "LAK":
-                SecondCurrencyType = rateKIP;
-                CurrencyName = "LAK";
-                break;
-        }
+  const amountElement = document.querySelector(".js-amount");
+  const resultElement = document.querySelector(".js-result");
 
-        const amountElementValue = amountElement.value;
+  const currencyFirstElement = document.querySelector(".js-currency-first");
+  const currencySecondElement = document.querySelector(".js-currency-second");
 
-        result = (amountElement.value * FirstCurrencyType / SecondCurrencyType).toLocaleString();
-        resultElement.innerText = `${amountElementValue} ${FirstCurrencyName} = ${result} ${CurrencyName}`;
+
+  currencyFirstElement.addEventListener("input", exchange);
+  currencySecondElement.addEventListener("input", exchange);
+  amountElement.addEventListener("input", exchange);
+
+
+  function enforce_maxlength(event) {
+    var t = event.target;
+    if (t.hasAttribute("maxlength")) {
+      t.value = t.value.slice(0, t.getAttribute("maxlength"));
     };
+  };
 
-    function enforce_maxlength(event) {
-        var t = event.target;
-        if (t.hasAttribute('maxlength')) {
-          t.value = t.value.slice(0, t.getAttribute('maxlength'));
-        }
-      }
-
-    document.body.addEventListener('input', enforce_maxlength);
-
-
-    currencyFirstElement.addEventListener("input", exchange);
-    currencySecondElement.addEventListener("input", exchange);
-    amountElement.addEventListener("input", exchange);
+  document.body.addEventListener("input", enforce_maxlength);
+}
